9b175368bf8ceb5558c799146ed6ffe6071a602f
Paulina Palmberg <paue97@gmail.com>
2018-09-11 13:14:56 +0200

    all done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,11 @@ public class Main {
 
         System.out.println("... with no real use!");
 
+        System.out.println("Can add a for-loop or something..");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Whatever " + i);
+        }
+
     }
 }
